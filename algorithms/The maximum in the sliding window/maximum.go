59044053510f5fd62d5c22594855989a16c00e49
Artem Zinoviev <arzinoviev@gmail.com>
2018-05-18 23:03:15 +0300

    using two stack

--- algorithms/The maximum in the sliding window/maximum.go
+++ algorithms/The maximum in the sliding window/maximum.go
@@ -4,46 +4,6 @@ import (
 	"fmt"
 )
 
-/**
-Максимум в скользящем окне
-	Найти максимум в каждом окне размера m данного массива чисел
-	A[1 . . . n].
-		Вход. Массив чисел A[1 . . . n] и число 1 ≤ m ≤ n.
-
-		Выход. Максимум подмассива A[i . . . i + m − 1] для всех 1 ≤ i ≤ n − m + 1.
-
-	===========
-	Формат входа. Первая строка входа содержит число n, вторая — мас-
-		сив A[1 . . . n], третья — число m.
-
-	Формат выхода. n − m + 1 максимумов, разделённых пробелами.
-
-	Ограниченяи.
-		1 ≤ n ≤ 10 5 , 1 ≤ m ≤ n, 0 ≤ A[i] ≤ 10 5 для всех 1 ≤ i ≤ n.
-
-	Примеры.
-		Вход:
-			8
-			2 7 3 1 5 2 6 2
-			4
-		Выход:
-			7 7 5 6 6
-
-		Вход:
-			3
-			2 1 5
-			1
-		Выход:
-			2 1 5
-
-		Вход:
-			3
-			2 3 9
-			3
-		Выход:
-			9
-*/
-
 func main() {
 	var (
 		n int
@@ -57,31 +17,57 @@ func main() {
 	fmt.Scan(&m)
 
 	for _, val := range process(n, data, m) {
-		fmt.Print(fmt.Sprintf("%v ", val))
+		fmt.Print(val, " ")
 	}
 	fmt.Println()
 }
 
 func process(n int, d []int, m int) (r []int) {
-	var queue []int
-	r = make([]int, 0, n-m+1)
+	in := make([]int, 0, m)
+	out := make([]int, 0, m)
+	r = make([]int, 0, m)
 
-	for i := 0; i < n-m+1; i++ {
-		queue = d[i : m+i]
-		for _, v := range queue {
-			if i >= len(r) {
-				r = append(r, v)
-				continue
-			}
-			if r[i] < v {
-				r[i] = v
+	for i := 0; i < m; i++ {
+		in = append(in, d[0])
+		d = d[1:]
+	}
+
+	for _, val := range in {
+		out = append(out, val)
+	}
+	in = in[:0]
+	r = append(r, max(out))
+
+	for _, val := range d {
+		in = append(in, val)
+
+		if len(out) == 0 {
+			for len(in) > 0 {
+				out = append(out, in[0])
+				in = in[1:]
 			}
 		}
+		out = out[1:]
+
+		mo := max(out)
+		mi := max(in)
+		if mo > mi {
+			r = append(r, mo)
+		} else {
+			r = append(r, mi)
+		}
+		d = d[1:]
 	}
+
 	return
 }
 
-// goos: linux
-// goarch: amd64
-// BenchmarkFlipAndInvertImage-2   	20000000	       108 ns/op
-// PASS
+func max(d []int) int {
+	m := 0
+	for i := range d {
+		if d[i] > m {
+			m = d[i]
+		}
+	}
+	return m
+}
