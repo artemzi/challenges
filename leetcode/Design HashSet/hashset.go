368f99d1c53ff1a753c36fe2fcaeb275cb0ea87c
Artem Zinoviev <arzinoviev@gmail.com>
2018-06-02 13:29:58 +0300

    add problem; setup metalinter rules

---
+++ leetcode/Design HashSet/hashset.go
@@ -0,0 +1,32 @@
+package hashset
+
+type MyHashSet struct {
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyHashSet {
+
+	return MyHashSet{}
+}
+
+func (m *MyHashSet) Add(key int) {
+
+}
+
+func (m *MyHashSet) Remove(key int) {
+
+}
+
+/** Returns true if this set did not already contain the specified element */
+func (m *MyHashSet) Contains(key int) bool {
+
+	return true
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * param_3 := obj.Contains(key);
+ */
