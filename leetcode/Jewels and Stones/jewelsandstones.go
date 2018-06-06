5b85db63c9658fc74553fef38f27830734e734a6
Artem Zinoviev <arzinoviev@gmail.com>
2018-06-06 10:30:23 +0300

    accepted; all tests passed

--- leetcode/Jewels and Stones/jewelsandstones.go
+++ leetcode/Jewels and Stones/jewelsandstones.go
@@ -19,7 +19,50 @@ You're given strings J representing the types of stones that are jewels, and S r
 		* The characters in J are distinct.
 */
 
-func numJewelsInStones(J string, S string) int {
+type MyHashMap struct {
+	val map[rune]int
+}
 
+/** Initialize your data structure here. */
+func Constructor() MyHashMap {
+	return MyHashMap{
+		make(map[rune]int),
+	}
+}
+
+/** value will always be positive. */
+func (m *MyHashMap) Put(key rune, value int) {
+	m.val[key] = value
+}
+
+/** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+func (m *MyHashMap) Get(key rune) int {
+	if val, ok := m.val[key]; ok {
+		return val
+	}
 	return -1
+}
+
+/** Removes the mapping of the specified value key if this map contains a mapping for the key */
+func (m *MyHashMap) Remove(key rune) {
+	delete(m.val, key)
+}
+
+func numJewelsInStones(J string, S string) int {
+	var result int
+	m := Constructor()
+	for _, r := range J {
+		m.Put(r, 0)
+	}
+
+	for _, r := range S {
+		if ok := m.Get(r); ok != -1 {
+			m.val[r]++
+		}
+	}
+
+	for _, v := range m.val {
+		result += v
+	}
+	return result
 }
